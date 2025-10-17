93a134c3d0fcc58db1496af9ebe2293e48085cb8
Victor Lorencio <victorlorencio@gmail.com>
2025-10-17 12:08:52 +0200

    Add new array of services

---
+++ src/consts/galleryBottles.ts
@@ -0,0 +1,17 @@
+import GalleryImg1 from "@/assets/galleryBottles/bottles_01.webp"
+import GalleryImg2 from "@/assets/galleryBottles/bottles_02.webp"
+import GalleryImg3 from "@/assets/galleryBottles/bottles_03.webp"
+import GalleryImg4 from "@/assets/galleryBottles/bottles_04.webp"
+import GalleryImg5 from "@/assets/galleryBottles/bottles_05.webp"
+import GalleryImg6 from "@/assets/galleryBottles/bottles_06.webp"
+import GalleryImg7 from "@/assets/galleryBottles/bottles_07.webp"
+
+export const galleryBottles = [
+    { image: GalleryImg1, alt: "Botellas personalizadas para eventos", thumb: "thumb1" },
+    { image: GalleryImg2, alt: "Botellas personalizadas para eventos", thumb: "thumb2" },
+    { image: GalleryImg3, alt: "Botellas personalizadas para eventos", thumb: "thumb3" },
+    { image: GalleryImg4, alt: "Botellas personalizadas para eventos", thumb: "thumb4" },
+    { image: GalleryImg5, alt: "Botellas personalizadas para eventos", thumb: "thumb5" },
+    { image: GalleryImg6, alt: "Botellas personalizadas para eventos", thumb: "thumb6" },
+    { image: GalleryImg7, alt: "Botellas personalizadas para eventos", thumb: "thumb7" }
+]
---
+++ src/consts/galleryPotatoes.ts
@@ -0,0 +1,17 @@
+import GalleryImg1 from "@/assets/galleryPotatoes/potatoe_01.webp"
+import GalleryImg2 from "@/assets/galleryPotatoes/potatoe_02.webp"
+import GalleryImg3 from "@/assets/galleryPotatoes/potatoe_03.webp"
+import GalleryImg4 from "@/assets/galleryPotatoes/potatoe_04.webp"
+import GalleryImg5 from "@/assets/galleryPotatoes/potatoe_05.webp"
+import GalleryImg6 from "@/assets/galleryPotatoes/potatoe_06.webp"
+import GalleryImg7 from "@/assets/galleryPotatoes/potatoe_07.webp"
+
+export const galleryPotatoes = [
+    { image: GalleryImg1, alt: "Patatas personalizadas para eventos", thumb: "thumb1" },
+    { image: GalleryImg2, alt: "Patatas personalizadas para eventos", thumb: "thumb2" },
+    { image: GalleryImg3, alt: "Patatas personalizadas para eventos", thumb: "thumb3" },
+    { image: GalleryImg4, alt: "Patatas personalizadas para eventos", thumb: "thumb4" },
+    { image: GalleryImg5, alt: "Patatas personalizadas para eventos", thumb: "thumb5" },
+    { image: GalleryImg6, alt: "Patatas personalizadas para eventos", thumb: "thumb6" },
+    { image: GalleryImg7, alt: "Patatas personalizadas para eventos", thumb: "thumb7" }
+]
