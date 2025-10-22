a44452dfb3dfc79ec0b070acb7cab2f4d289fb95
Victor Lorencio <victorlorencio@gmail.com>
2025-10-22 10:01:15 +0200

    Add lollipop gallery and include it in your stationery.

---
+++ src/consts/galleryLollipops.ts
@@ -0,0 +1,47 @@
+import GalleryLollipops1 from "@/assets/galleryLollipops/lollipops_01.webp";
+import GalleryLollipops2 from "@/assets/galleryLollipops/lollipops_02.webp";
+import GalleryLollipops3 from "@/assets/galleryLollipops/lollipops_03.webp";
+import GalleryLollipops4 from "@/assets/galleryLollipops/lollipops_04.webp";
+import GalleryLollipops5 from "@/assets/galleryLollipops/lollipops_05.webp";
+import GalleryLollipops6 from "@/assets/galleryLollipops/lollipops_06.webp";
+import GalleryLollipops7 from "@/assets/galleryLollipops/lollipops_07.webp";
+import GalleryLollipops8 from "@/assets/galleryLollipops/lollipops_08.webp";
+import GalleryLollipops9 from "@/assets/galleryLollipops/lollipops_09.webp";
+import GalleryLollipops10 from "@/assets/galleryLollipops/lollipops_10.webp";
+import GalleryLollipops11 from "@/assets/galleryLollipops/lollipops_11.webp";
+import GalleryLollipops12 from "@/assets/galleryLollipops/lollipops_12.webp";
+import GalleryLollipops13 from "@/assets/galleryLollipops/lollipops_13.webp";
+import GalleryLollipops14 from "@/assets/galleryLollipops/lollipops_14.webp";
+import GalleryLollipops15 from "@/assets/galleryLollipops/lollipops_15.webp";
+import GalleryLollipops16 from "@/assets/galleryLollipops/lollipops_16.webp";
+import GalleryLollipops17 from "@/assets/galleryLollipops/lollipops_17.webp";
+import GalleryLollipops18 from "@/assets/galleryLollipops/lollipops_18.webp";
+import GalleryLollipops19 from "@/assets/galleryLollipops/lollipops_19.webp";
+import GalleryLollipops20 from "@/assets/galleryLollipops/lollipops_20.webp";
+
+
+
+
+
+export const galleryLollipops = [
+    { image: GalleryLollipops1, alt: "Chupachups personalizados para eventos", thumb: "thumb1" },
+    { image: GalleryLollipops2, alt: "Chupachups personalizados para eventos", thumb: "thumb2" },
+    { image: GalleryLollipops3, alt: "Chupachups personalizados para eventos", thumb: "thumb3" },
+    { image: GalleryLollipops4, alt: "Chupachups personalizados para eventos", thumb: "thumb4" },
+    { image: GalleryLollipops5, alt: "Chupachups personalizados para eventos", thumb: "thumb5" },
+    { image: GalleryLollipops6, alt: "Chupachups personalizados para eventos", thumb: "thumb6" },
+    { image: GalleryLollipops7, alt: "Chupachups personalizados para eventos", thumb: "thumb7" },
+    { image: GalleryLollipops8, alt: "Chupachups personalizados para eventos", thumb: "thumb8" },
+    { image: GalleryLollipops9, alt: "Chupachups personalizados para eventos", thumb: "thumb9" },
+    { image: GalleryLollipops10, alt: "Chupachups personalizados para eventos", thumb: "thumb10" },
+    { image: GalleryLollipops11, alt: "Chupachups personalizados para eventos", thumb: "thumb11" },
+    { image: GalleryLollipops12, alt: "Chupachups personalizados para eventos", thumb: "thumb12" },
+    { image: GalleryLollipops13, alt: "Chupachups personalizados para eventos", thumb: "thumb13" },
+    { image: GalleryLollipops14, alt: "Chupachups personalizados para eventos", thumb: "thumb14" },
+    { image: GalleryLollipops15, alt: "Chupachups personalizados para eventos", thumb: "thumb15" },
+    { image: GalleryLollipops16, alt: "Chupachups personalizados para eventos", thumb: "thumb16" },
+    { image: GalleryLollipops17, alt: "Chupachups personalizados para eventos", thumb: "thumb17" },
+    { image: GalleryLollipops18, alt: "Chupachups personalizados para eventos", thumb: "thumb18" },
+    { image: GalleryLollipops19, alt: "Chupachups personalizados para eventos", thumb: "thumb19" },
+    { image: GalleryLollipops20, alt: "Chupachups personalizados para eventos", thumb: "thumb20" }
+]
