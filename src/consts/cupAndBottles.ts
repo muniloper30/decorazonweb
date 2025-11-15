913862ae6cde89d98aae1aaccaa0c74ef92b1897
Victor Lorencio <victorlorencio@gmail.com>
2025-11-15 21:17:07 +0100

    minor changes

--- src/consts/cupAndBottles.ts
+++ src/consts/cupAndBottles.ts
@@ -10,6 +10,14 @@ import GalleryImg9 from "@/assets/giftGallery/cupsAndBottles/tazbot_09.webp"
 import GalleryImg10 from "@/assets/giftGallery/cupsAndBottles/tazbot_10.webp"
 import GalleryImg11 from "@/assets/giftGallery/cupsAndBottles/tazbot_11.webp"
 import GalleryImg12 from "@/assets/giftGallery/cupsAndBottles/tazbot_12.webp"
+import GalleryImg13 from "@/assets/giftGallery/cupsAndBottles/tazbot_13.webp"
+import GalleryImg14 from "@/assets/giftGallery/cupsAndBottles/tazbot_14.webp"
+import GalleryImg15 from "@/assets/giftGallery/cupsAndBottles/tazbot_15.webp"
+import GalleryImg16 from "@/assets/giftGallery/cupsAndBottles/tazbot_16.webp"
+import GalleryImg17 from "@/assets/giftGallery/cupsAndBottles/tazbot_17.webp"
+import GalleryImg18 from "@/assets/giftGallery/cupsAndBottles/tazbot_18.webp"
+import GalleryImg19 from "@/assets/giftGallery/cupsAndBottles/tazbot_19.webp"
+import GalleryImg20 from "@/assets/giftGallery/cupsAndBottles/tazbot_20.webp"     
 
 export const cupAndBottles = [
     { image: GalleryImg1, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb1" },
@@ -24,5 +32,14 @@ export const cupAndBottles = [
     { image: GalleryImg10, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb10" },
     { image: GalleryImg11, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb11" },
     { image: GalleryImg12, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb12" },
+    { image: GalleryImg13, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb13" },
+    { image: GalleryImg14, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb14" },
+    { image: GalleryImg15, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb15" },
+    { image: GalleryImg16, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb16" },
+    { image: GalleryImg17, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb17" },
+    { image: GalleryImg18, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb18" },
+    { image: GalleryImg19, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb19" },
+    { image: GalleryImg20, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb20" }
+    
     
 ]
