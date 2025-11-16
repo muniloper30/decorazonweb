afd599d964140736970f567f3ecc6374240b1407
Victor Lorencio <victorlorencio@gmail.com>
2025-11-16 18:13:34 +0100

    add assets of mother-father and include files on detallesRegalos

---
+++ src/consts/motherGifts.ts
@@ -0,0 +1,17 @@
+import GalleryImg1 from "@/assets/giftGallery/motherGifts/motherGift_01.webp"
+import GalleryImg2 from "@/assets/giftGallery/motherGifts/motherGift_02.webp"
+import GalleryImg3 from "@/assets/giftGallery/motherGifts/motherGift_03.webp"
+import GalleryImg4 from "@/assets/giftGallery/motherGifts/motherGift_04.webp"
+import GalleryImg5 from "@/assets/giftGallery/motherGifts/motherGift_05.webp"
+import GalleryImg6 from "@/assets/giftGallery/motherGifts/motherGift_06.webp"
+   
+
+export const motherGifts = [
+    { image: GalleryImg1, alt: "Regalos para el día de la madre", thumb: "thumb1" },
+    { image: GalleryImg2, alt: "Regalos para el día de la madre", thumb: "thumb2" },
+    { image: GalleryImg3, alt: "Regalos para el día de la madre", thumb: "thumb3" },
+    { image: GalleryImg4, alt: "Regalos para el día de la madre", thumb: "thumb4" },
+    { image: GalleryImg5, alt: "Regalos para el día de la madre", thumb: "thumb5" },
+    { image: GalleryImg6, alt: "Regalos para el día de la madre", thumb: "thumb6" },
+    
+]
