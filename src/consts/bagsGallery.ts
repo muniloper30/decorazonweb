f2d3a723bd8766c9693f5ee9ca6b35f97e3d0fa1
Victor Lorencio <victorlorencio@gmail.com>
2025-11-16 17:40:55 +0100

    add assets bags and implements on detallesRegalos

--- src/consts/bagsGallery.ts
+++ src/consts/bagsGallery.ts
@@ -1,21 +1,35 @@
-import GalleryImg1 from "@/assets/bagsGallery/bags/bag_01.webp"
-import GalleryImg2 from "@/assets/bagsGallery/bags/bag_02.webp"
-import GalleryImg3 from "@/assets/bagsGallery/bags/bag_03.webp"
-import GalleryImg4 from "@/assets/bagsGallery/bags/bag_04.webp"
-import GalleryImg5 from "@/assets/bagsGallery/bags/bag_05.webp"
-import GalleryImg6 from "@/assets/bagsGallery/bags/bag_06.webp"
-import GalleryImg7 from "@/assets/bagsGallery/bags/bag_07.webp"
-import GalleryImg8 from "@/assets/bagsGallery/bags/bag_08.webp"
-import GalleryImg9 from "@/assets/bagsGallery/bags/bag_09.webp"
+import GalleryImg1 from "@/assets/giftGallery/bags/bag_01.webp"
+import GalleryImg2 from "@/assets/giftGallery/bags/bag_02.webp"
+import GalleryImg3 from "@/assets/giftGallery/bags/bag_03.webp"
+import GalleryImg4 from "@/assets/giftGallery/bags/bag_04.webp"
+import GalleryImg5 from "@/assets/giftGallery/bags/bag_05.webp"
+import GalleryImg6 from "@/assets/giftGallery/bags/bag_06.webp"
+import GalleryImg7 from "@/assets/giftGallery/bags/bag_07.webp"
+import GalleryImg8 from "@/assets/giftGallery/bags/bag_08.webp"
+import GalleryImg9 from "@/assets/giftGallery/bags/bag_09.webp"
+import GalleryImg10 from "@/assets/giftGallery/bags/bag_10.webp"
+import GalleryImg11 from "@/assets/giftGallery/bags/bag_11.webp"
+import GalleryImg12 from "@/assets/giftGallery/bags/bag_12.webp"
+import GalleryImg13 from "@/assets/giftGallery/bags/bag_13.webp"
+import GalleryImg14 from "@/assets/giftGallery/bags/bag_14.webp"
+import GalleryImg15 from "@/assets/giftGallery/bags/bag_15.webp"
 
-export const cupAndBottles = [
-    { image: GalleryImg1, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb1" },
-    { image: GalleryImg2, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb2" },
-    { image: GalleryImg3, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb3" },
-    { image: GalleryImg4, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb4" },
-    { image: GalleryImg5, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb5" },
-    { image: GalleryImg6, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb6" },
-    { image: GalleryImg7, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb7" },
-    { image: GalleryImg8, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb8" },
-    { image: GalleryImg9, alt: "Tazas y botellas personalizadas para eventos", thumb: "thumb9" }
+
+
+export const bagsGallery = [
+    { image: GalleryImg1, alt: "Bolsos personalizados para eventos", thumb: "thumb1" },
+    { image: GalleryImg2, alt: "Bolsos personalizados para eventos", thumb: "thumb2" },
+    { image: GalleryImg3, alt: "Bolsos personalizados para eventos", thumb: "thumb3" },
+    { image: GalleryImg4, alt: "Bolsos personalizados para eventos", thumb: "thumb4" },
+    { image: GalleryImg5, alt: "Bolsos personalizados para eventos", thumb: "thumb5" },
+    { image: GalleryImg6, alt: "Bolsos personalizados para eventos", thumb: "thumb6" },
+    { image: GalleryImg7, alt: "Bolsos personalizados para eventos", thumb: "thumb7" },
+    { image: GalleryImg8, alt: "Bolsos personalizados para eventos", thumb: "thumb8" },
+    { image: GalleryImg9, alt: "Bolsos personalizados para eventos", thumb: "thumb9" },
+    { image: GalleryImg10, alt: "Bolsos personalizados para eventos", thumb: "thumb10" },
+    { image: GalleryImg11, alt: "Bolsos personalizados para eventos", thumb: "thumb11" },
+    { image: GalleryImg12, alt: "Bolsos personalizados para eventos", thumb: "thumb12" },
+    { image: GalleryImg13, alt: "Bolsos personalizados para eventos", thumb: "thumb13" },
+    { image: GalleryImg14, alt: "Bolsos personalizados para eventos", thumb: "thumb14" },
+    { image: GalleryImg15, alt: "Bolsos personalizados para eventos", thumb: "thumb15" },
 ]
