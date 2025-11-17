d0d325868154e687d055be2624dc3e335b062b08
Victor Lorencio <victorlorencio@gmail.com>
2025-11-17 09:00:17 +0100

    delete hidden sections and add christmas assets

---
+++ src/consts/christmasGifts.ts
@@ -0,0 +1,29 @@
+import GalleryImg1 from '@/assets/giftGallery/christmas/christmas_01.webp';
+import GalleryImg2 from '@/assets/giftGallery/christmas/christmas_02.webp';
+import GalleryImg3 from '@/assets/giftGallery/christmas/christmas_03.webp';
+import GalleryImg4 from '@/assets/giftGallery/christmas/christmas_04.webp';
+import GalleryImg5 from '@/assets/giftGallery/christmas/christmas_05.webp';
+import GalleryImg6 from '@/assets/giftGallery/christmas/christmas_06.webp';
+import GalleryImg7 from '@/assets/giftGallery/christmas/christmas_07.webp';
+import GalleryImg8 from '@/assets/giftGallery/christmas/christmas_08.webp';
+import GalleryImg9 from '@/assets/giftGallery/christmas/christmas_09.webp';
+import GalleryImg10 from '@/assets/giftGallery/christmas/christmas_10.webp';
+import GalleryImg11 from '@/assets/giftGallery/christmas/christmas_11.webp';
+import GalleryImg12 from '@/assets/giftGallery/christmas/christmas_12.webp';
+import GalleryImg13 from '@/assets/giftGallery/christmas/christmas_13.webp';
+
+export const christmasGifts = [
+    { image: GalleryImg1, alt: 'Regalos navideños personalizados', thumb: 'thumb1' },
+    { image: GalleryImg2, alt: 'Regalos navideños personalizados', thumb: 'thumb2' },
+    { image: GalleryImg3, alt: 'Regalos navideños personalizados', thumb: 'thumb3' },
+    { image: GalleryImg4, alt: 'Regalos navideños personalizados', thumb: 'thumb4' },
+    { image: GalleryImg5, alt: 'Regalos navideños personalizados', thumb: 'thumb5' },
+    { image: GalleryImg6, alt: 'Regalos navideños personalizados', thumb: 'thumb6' },
+    { image: GalleryImg7, alt: 'Regalos navideños personalizados', thumb: 'thumb7' },
+    { image: GalleryImg8, alt: 'Regalos navideños personalizados', thumb: 'thumb8' },
+    { image: GalleryImg9, alt: 'Regalos navideños personalizados', thumb: 'thumb9' },
+    { image: GalleryImg10, alt: 'Regalos navideños personalizados', thumb: 'thumb10' },
+    { image: GalleryImg11, alt: 'Regalos navideños personalizados', thumb: 'thumb11' },
+    { image: GalleryImg12, alt: 'Regalos navideños personalizados', thumb: 'thumb12' },
+    { image: GalleryImg13, alt: 'Regalos navideños personalizados', thumb: 'thumb13' },
+];
