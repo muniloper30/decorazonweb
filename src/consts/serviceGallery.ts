af7b26433b51d5c72edbce368e52a1be43476b1e
Victor Lorencio <victorlorencio@gmail.com>
2025-12-17 13:22:14 +0100

    concetar celebraciones.astro a const of services

--- src/consts/serviceGallery.ts
+++ src/consts/serviceGallery.ts
@@ -4,6 +4,10 @@ import ServiceImg3 from '@/assets/servicios/montajeL.webp';
 import ServiceImg4 from '@/assets/servicios/montajeXL.webp';
 import ServiceImg5 from '@/assets/servicios/montajeGlobo.webp';
 import ServiceImg6 from '@/assets/servicios/mesasInfantiles.webp';
+import ServiceImg7 from '@/assets/servicios/revelacion.webp';
+import ServiceImg8 from '@/assets/servicios/babyshower.webp';
+import ServiceImg9 from '@/assets/servicios/cumpleanios1.webp';
+import ServiceImg10 from '@/assets/servicios/comunion1.webp'; 
 
 export const serviceGallery = [
     { image: ServiceImg1, alt: 'Montaje Pequeño (S)', thumb: 'thumb1' },
@@ -12,4 +16,8 @@ export const serviceGallery = [
     { image: ServiceImg4, alt: 'Montaje Extra Grande (XL)', thumb: 'thumb4' },
     { image: ServiceImg5, alt: 'Montaje con Globos', thumb: 'thumb5' },
     { image: ServiceImg6, alt: 'Mesas Infantiles', thumb: 'thumb6' },
+    { image: ServiceImg7, alt: 'Revelación de Género', thumb: 'thumb7' },
+    { image: ServiceImg8, alt: 'Baby Shower', thumb: 'thumb8' },
+    { image: ServiceImg9, alt: 'Cumpleaños', thumb: 'thumb9' },
+    { image: ServiceImg10, alt: 'Primera Comunión', thumb: 'thumb10' },
 ];
