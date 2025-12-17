a14e85316e946f61a82199ceb68d5a4438707784
Victor Lorencio <victorlorencio@gmail.com>
2025-12-17 13:15:37 +0100

    incluir serviceGallery y conectar con montajesdeMesas

---
+++ src/consts/serviceGallery.ts
@@ -0,0 +1,15 @@
+import ServiceImg1 from '@/assets/servicios/montajeS.webp';
+import ServiceImg2 from '@/assets/servicios/montajeM.webp';
+import ServiceImg3 from '@/assets/servicios/montajeL.webp';
+import ServiceImg4 from '@/assets/servicios/montajeXL.webp';
+import ServiceImg5 from '@/assets/servicios/montajeGlobo.webp';
+import ServiceImg6 from '@/assets/servicios/mesasInfantiles.webp';
+
+export const serviceGallery = [
+    { image: ServiceImg1, alt: 'Montaje Pequeño (S)', thumb: 'thumb1' },
+    { image: ServiceImg2, alt: 'Montaje Mediano (M)', thumb: 'thumb2' },
+    { image: ServiceImg3, alt: 'Montaje Grande (L)', thumb: 'thumb3' },
+    { image: ServiceImg4, alt: 'Montaje Extra Grande (XL)', thumb: 'thumb4' },
+    { image: ServiceImg5, alt: 'Montaje con Globos', thumb: 'thumb5' },
+    { image: ServiceImg6, alt: 'Mesas Infantiles', thumb: 'thumb6' },
+];
