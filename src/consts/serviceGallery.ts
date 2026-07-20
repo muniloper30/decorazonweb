c8e990c36131b744f1121121fb7de08c9c4c8466
Victor Lorencio <victorlorencio@gmail.com>
2026-07-20 09:20:41 +0200

    feature:Add new table size, sweet/savory setup & update images

--- src/consts/serviceGallery.ts
+++ src/consts/serviceGallery.ts
@@ -1,23 +1,32 @@
-import ServiceImg1 from '@/assets/servicios/montajeS.webp';
-import ServiceImg2 from '@/assets/servicios/montajeM.webp';
-import ServiceImg3 from '@/assets/servicios/montajeL.webp';
-import ServiceImg4 from '@/assets/servicios/montajeXL.webp';
-import ServiceImg5 from '@/assets/servicios/montajeGlobo.webp';
-import ServiceImg6 from '@/assets/servicios/mesasInfantiles.webp';
-import ServiceImg7 from '@/assets/servicios/revelacion.webp';
-import ServiceImg8 from '@/assets/servicios/babyshower.webp';
-import ServiceImg9 from '@/assets/servicios/cumpleanios1.webp';
-import ServiceImg10 from '@/assets/servicios/comunion1.webp'; 
+import ServiceImg1 from "@/assets/servicios/montajeS.webp";
+import ServiceImg2 from "@/assets/servicios/montajeM.webp";
+import ServiceImg3 from "@/assets/servicios/montajeL.webp";
+import ServiceImg4 from "@/assets/servicios/montajeXL.webp";
+import ServiceImg5 from "@/assets/servicios/montajeGlobo.webp";
+import ServiceImg6 from "@/assets/servicios/mesasInfantiles.webp";
+import ServiceImg7 from "@/assets/servicios/revelacion.webp";
+import ServiceImg8 from "@/assets/servicios/babyshower.webp";
+import ServiceImg9 from "@/assets/servicios/cumpleanios1.webp";
+import ServiceImg10 from "@/assets/servicios/comunion1.webp";
+import ServiceImg11 from "@/assets/servicios/montaje_XS.webp";
+import ServiceImg12 from "@/assets/servicios/montaje_S.webp";
+import ServiceImg13 from "@/assets/servicios/montaje_M.webp";
+import ServiceImg14 from "@/assets/servicios/montaje_L.webp";
+import ServiceImg15 from "@/assets/servicios/montaje_XL.webp";
+import ServiceImg16 from "@/assets/servicios/mesas_infantiles.webp";
+import ServiceImg17 from "@/assets/servicios/mesas_dulces.webp";
 
 export const serviceGallery = [
-    { image: ServiceImg1, alt: 'Montaje Pequeño (S)', thumb: 'thumb1' },
-    { image: ServiceImg2, alt: 'Montaje Mediano (M)', thumb: 'thumb2' },
-    { image: ServiceImg3, alt: 'Montaje Grande (L)', thumb: 'thumb3' },
-    { image: ServiceImg4, alt: 'Montaje Extra Grande (XL)', thumb: 'thumb4' },
-    { image: ServiceImg5, alt: 'Montaje con Globos', thumb: 'thumb5' },
-    { image: ServiceImg6, alt: 'Mesas Infantiles', thumb: 'thumb6' },
-    { image: ServiceImg7, alt: 'Revelación de Género', thumb: 'thumb7' },
-    { image: ServiceImg8, alt: 'Baby Shower', thumb: 'thumb8' },
-    { image: ServiceImg9, alt: 'Cumpleaños', thumb: 'thumb9' },
-    { image: ServiceImg10, alt: 'Primera Comunión', thumb: 'thumb10' },
+  { image: ServiceImg11, alt: "Montaje Pequeño (XS)", thumb: "thumb11" },
+  { image: ServiceImg12, alt: "Montaje Pequeño (S)", thumb: "thumb12" },
+  { image: ServiceImg13, alt: "Montaje Mediano (M)", thumb: "thumb13" },
+  { image: ServiceImg14, alt: "Montaje Grande (L)", thumb: "thumb14" },
+  { image: ServiceImg15, alt: "Montaje Extra Grande (XL)", thumb: "thumb15" },
+  { image: ServiceImg5, alt: "Montaje con Globos", thumb: "thumb5" },
+  { image: ServiceImg16, alt: "Mesas Infantiles", thumb: "thumb16" },
+  { image: ServiceImg17, alt: "Mesas Dulces", thumb: "thumb17" },
+  { image: ServiceImg7, alt: "Revelación de Género", thumb: "thumb7" },
+  { image: ServiceImg8, alt: "Baby Shower", thumb: "thumb8" },
+  { image: ServiceImg9, alt: "Cumpleaños", thumb: "thumb9" },
+  { image: ServiceImg10, alt: "Primera Comunión", thumb: "thumb10" },
 ];
