4a28942870e656d4bd32318af8cd8bca2f081113
Victor Lorencio <victorlorencio@gmail.com>
2026-07-22 19:26:01 +0200

    minor changes in to services view

--- src/consts/servicios.ts
+++ src/consts/servicios.ts
@@ -3,17 +3,14 @@ import celebraciones from "../assets/servicios/celebraciones.webp";
 import papeleria from "../assets/servicios/papeleria.webp";
 import detalles from "../assets/servicios/detalles.webp";
 
-
-
 export const servicios = [
   {
     image: montajes,
     alt: "Montajes de mesas",
     title: "Montajes de mesas",
     url: "/MontajeMesas",
-    desc:
-      "Creamos mesas decorativas a medida de diferentes tamaños. Cuidamos cada detalle para lograr una decoración perfecta.",
-    tags: ["Montaje S", "Montaje M", "Montaje L", "Montaje XL", "Mesas infantiles"],
+    desc: "Creamos mesas decorativas a medida de diferentes tamaños. Cuidamos cada detalle para lograr una decoración perfecta.",
+    tags: ["Montaje (XS a XL)", "Mesas dulces y salado", "Mesas infantiles"],
     objectPos: "object-center",
   },
   {
@@ -21,9 +18,13 @@ export const servicios = [
     alt: "Decoración de celebraciones",
     title: "Celebraciones",
     url: "/Celebraciones",
-    desc:
-      "Diseñamos ambientes únicos y personalizados para que cada momento se convierta en un recuerdo inolvidable.",
-    tags: ["Revelaciones de sexo", "Babyshowers", "Cumpleaños", "Bautizos y Comuniones"],
+    desc: "Diseñamos ambientes únicos y personalizados para que cada momento se convierta en un recuerdo inolvidable.",
+    tags: [
+      "Revelaciones de sexo",
+      "Babyshowers",
+      "Cumpleaños",
+      "Bautizos y Comuniones",
+    ],
     objectPos: "object-center",
   },
   {
@@ -31,9 +32,15 @@ export const servicios = [
     alt: "Papelería personalizada",
     title: "Papelería",
     url: "/Papeleria",
-    desc:
-      "Botellas personalizadas, diferentes snacks personalizados, manteles, etiquetas, cartelería y decoración temática para aportar un toque exclusivo a tu celebración.",
-    tags: ["Botellas", "Papas", "Chocolatinas" , "Pringles", "Chupa chups" , "Nutellas"],
+    desc: "Botellas personalizadas, diferentes snacks personalizados, manteles, etiquetas, cartelería y decoración temática para aportar un toque exclusivo a tu celebración.",
+    tags: [
+      "Botellas",
+      "Papas",
+      "Chocolatinas",
+      "Pringles",
+      "Chupa chups",
+      "Nutellas",
+    ],
     objectPos: "object-center",
   },
   {
@@ -41,8 +48,7 @@ export const servicios = [
     alt: "Detalles y regalos personalizados",
     title: "Detalles y regalos",
     url: "/DetallesRegalos",
-    desc:
-      "Detalles únicos para el Día de la Madre, del Padre, Navidad u otras fechas. Pequeños recuerdos que transmiten cariño.",
+    desc: "Detalles únicos para el Día de la Madre, del Padre, Navidad u otras fechas. Pequeños recuerdos que transmiten cariño.",
     tags: ["Tazas", "Botellas", "Cajas regalo", "Navidad", "Bolsos"],
     objectPos: "object-center",
   },
